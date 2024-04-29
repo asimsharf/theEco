706f889300344b85b42b1d7e51b0d896ee4b69c4
asimsharf <asimsahrf@gmail.com>
2024-04-29 13:56:59 +0300

    secound commit.

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,9 +1,8 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
-
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
@@ -19,5 +18,40 @@ class HomeController extends GetxController {
     super.onClose();
   }
 
-  void increment() => count.value++;
+  final dio = Dio();
+
+  Future<void> fetchData() async {
+    try {
+      await dio.get('https://api.example.com/data').then(
+        (response) {
+          if (kDebugMode) {
+            print(response.data);
+          }
+        },
+      );
+    } catch (error) {
+      if (kDebugMode) {
+        print('Error fetching data: $error');
+      }
+    }
+  }
+
+  Future<void> postData() async {
+    try {
+      await dio.post(
+        'https://api.example.com/post',
+        data: {'key': 'value'},
+      ).then(
+        (value) {
+          if (kDebugMode) {
+            print(value.data);
+          }
+        },
+      );
+    } catch (error) {
+      if (kDebugMode) {
+        print('Error posting data: $error');
+      }
+    }
+  }
 }
